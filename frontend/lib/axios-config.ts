9cfdebdbb4440ed4a1f6cbcbe49b4ad581e9f675
Nguyễn Duy Vũ <132334654+NguyenVu04@users.noreply.github.com>
2024-11-26 22:21:22 +0700

    update

--- frontend/lib/axios-config.ts
+++ frontend/lib/axios-config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const spssAxios = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: process.env.BACKEND_URL || "http://localhost:8080",
     timeout: 4000
 })
