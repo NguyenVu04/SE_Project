70bae77b9262cd37705e838921d16d7ae7399538
Nguyễn Duy Vũ <132334654+NguyenVu04@users.noreply.github.com>
2024-11-28 16:36:12 +0700

    Update SPSSSecurityConfig.java

--- backend/src/main/java/spss/project/backend/configuration/security/SPSSSecurityConfig.java
+++ backend/src/main/java/spss/project/backend/configuration/security/SPSSSecurityConfig.java
@@ -2,24 +2,35 @@ package spss.project.backend.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 @EnableWebSecurity
 public class SPSSSecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll())
-                .logout((logout) -> logout.permitAll());
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests((requests) -> requests
+                                                .requestMatchers(HttpMethod.GET, "/document", "/report")
+                                                .permitAll()
+                                                .anyRequest()
+                                                .access(new WebExpressionAuthorizationManager(
+                                                                "hasIpAddress('127.0.0.1')")))
+                                .formLogin((form) -> form
+                                                .disable())
+                                .httpBasic(basic -> basic
+                                                .disable())
+                                .passwordManagement(password -> password
+                                                .disable())
+                                .logout((logout) -> logout
+                                                .permitAll())
+                                .csrf((csrf) -> csrf
+                                                .disable());
 
-        return http.build();
-    }
+                return http.build();
+        }
 }
