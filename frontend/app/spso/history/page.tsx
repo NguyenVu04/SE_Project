8b9311605f21105a13a47d976e4d483a9896ac49
Nguyễn Duy Vũ <132334654+NguyenVu04@users.noreply.github.com>
2024-12-01 16:11:35 +0700

    Update page.tsx

--- frontend/app/spso/history/page.tsx
+++ frontend/app/spso/history/page.tsx
@@ -121,6 +121,8 @@ export default function Home() {
                         setPrinterId("");
                         setPrinterHistoryFrom("");
                         setPrinterHistoryTo("");
+                        setStudentHistory([]);
+                        setPrinterHistory([]);
                     }}
                         className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded mt-2"
                         >
