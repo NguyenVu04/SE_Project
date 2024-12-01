e10e3ef6ee40f1b6fdf45cdece485275df1d95f0
Vu Lam <114861830+Vu-Lam@users.noreply.github.com>
2024-12-01 09:48:59 +0700

    css spso/history

--- frontend/components/ui/topbar.tsx
+++ frontend/components/ui/topbar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { FaRegUserCircle } from "react-icons/fa";
 import { useRouter } from 'next/navigation'; 
 import { usePathname } from 'next/navigation'
-
+import Link from 'next/link';
 const TopBar = () => {
   const router = useRouter() // Initialize the router instance
   const currentPath = usePathname() // Get the current pathname
@@ -15,7 +15,9 @@ const TopBar = () => {
 
   return (
     <div className="flex items-center justify-between p-4 py-2 mb-2 top-0">
-      <Image src="/640px-HCMUT_official_logo.png" alt="logo-hcmut" width={50} height={50} />
+      <Link href="/spso">
+        <Image src="/640px-HCMUT_official_logo.png" alt="logo-hcmut" width={50} height={50} className='cursor-pointer'/>
+      </Link>
       <FaRegUserCircle
         size={40}
         className="text-4xl text-hcmut-dark cursor-pointer"
